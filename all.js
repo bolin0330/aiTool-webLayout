994fa947244527d613794a52cd234848d588d420
bolin0330 <bolin0330@gmail.com>
2023-08-15 22:17:35 +0800

    renew

--- all.js
+++ all.js
@@ -1,10 +1,32 @@
+$(function () {
+  $('.navbar-toggler').click(function () {
+    $('.navbar-collapse').toggleClass('show');
+  });
+  $('.dropdown').mouseenter(function () {
+    $(this).find('.dropdown-menu').stop().slideDown(100);
+  });
+  $('.dropdown').mouseleave(function () {
+    $(this).find('.dropdown-menu').stop().slideUp(100);
+  });
+  $('.filter-list-selection').click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass('active').siblings().removeClass('active');
+  });
+  $('.page-gui').click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass('active').siblings().removeClass('active');
+  });
+  $('#btn-top').click(function () {
+    $('html').animate({scrollTop: 0}, 1000);
+  })
+});
+
 const enterpriseSwiper = new Swiper ( ".enterprise-swiper", {
     slidesPerView: 'auto',
     spaceBetween: 20,
     freeMode: true,
     grabCursor: true
 })
-
 
 const commentSwipper = new Swiper(".comment-swiper", {
   slidesPerView: 1,
