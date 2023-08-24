aaa7cede58721d4df6faaa2ce353c1bafd60628f
bolin0330 <bolin0330@gmail.com>
2023-08-24 21:20:58 +0800

    renew

--- all.js
+++ all.js
@@ -59,3 +59,63 @@ const commentSwipper = new Swiper(".comment-swiper", {
     clickable: true
   }
 })
+
+const apiPath = 'https://2023-engineer-camp.zeabur.app';
+const list = document.querySelector('#list');
+const pagination = document.querySelector('#pagination');
+
+const data = {
+  type: '',
+  sort: 0,
+  page: 1,
+  search: '',
+}
+
+let worksData = [];
+let pagesData = {};
+
+function getData({ type, sort, page, search }) {
+  const apiUrl = `${apiPath}/api/v1/works?sort=${sort}&page=${page}&${type ? `type=${type}&` : ''}${search ? `search=${search}` : ''}`
+  axios.get(apiUrl)
+    .then(function(res) {
+      worksData = res.data.ai_works.data;
+      pagesData = res.data.ai_works.page;
+
+      renderWorks();
+      renderPages();
+    })
+}
+
+getData(data);
+
+function renderWorks() {
+  let works = '';
+
+  worksData.forEach(function(item) {
+    works += /*html*/`<li class="col-4 card">
+      <div class="h-100 card-frame">
+        <div class="card-exhibit">
+          <img class="w-100 card-zooming" src="${item.imageUrl}" alt="ai image">
+        </div>
+        <div class="card-context">
+          <h3 class="h5 mb-12">${item.title}</h3>
+          <p class="text-sm text-gray">${item.description}</p>
+        </div>
+        <div class="card-drawer">
+          <h4 class="h6">AI 模型</h4>
+          <p>${item.model}</p>
+        </div>
+        <div class="card-drawer">
+          <span>#${item.type}</span>
+          <a href="${item.link}" target="_blank">
+            <span class="material-icons">
+              share
+            </span>
+          </a>
+        </div>
+      </div>
+    </li>`
+  });
+
+  list.innerHTML = works;
+}
